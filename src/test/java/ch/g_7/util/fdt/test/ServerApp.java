345f11b1261e14be40f68b4958f2120bf0a29e98
Joey Sciamanna <Joey Sciamanna@DESKTOP-MGFM0VH>
2019-09-28 12:55:07 +0200

    .

--- src/test/java/ch/g_7/util/fdt/test/ServerApp.java
+++ src/test/java/ch/g_7/util/fdt/test/ServerApp.java
@@ -1,5 +1,7 @@
 package ch.g_7.util.fdt.test;
 
+import java.net.InetAddress;
+
 import ch.g_7.util.fdt.FDTServer;
 import ch.g_7.util.fdt.data.Metadata;
 import ch.g_7.util.fdt.exception.FDTException;
@@ -9,6 +11,7 @@ public class ServerApp {
 
 
 	public static void main(String[] args) throws Exception {
+		System.out.println(InetAddress.getLocalHost());
 		FDTServer server = new FDTServer(4004);
 		server.open();
 		server.add(new StringReciever() {
