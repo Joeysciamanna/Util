7ccfd568e1324bf94ca90eb6ea78548ba6ab400e
Joey Sciamanna <Joey Sciamanna@DESKTOP-MGFM0VH.home>
2019-10-05 13:13:26 +0200

    StreamUtil

--- src/main/java/ch/g_7/util/fdt/function/Reciever.java
+++ src/main/java/ch/g_7/util/fdt/function/Reciever.java
@@ -1,9 +1,10 @@
 package ch.g_7.util.fdt.function;
 
 import ch.g_7.util.fdt.data.Request;
+import ch.g_7.util.fdt.data.Response;
 import ch.g_7.util.fdt.exception.FDTException;
 
 public abstract class Reciever extends Function {
 
-	public abstract String recieve(Request request) throws FDTException;
+	public abstract Response recieve(Request request) throws FDTException;
 }
