8e18de6f546d456ca835e6bdf1d8298f51d92a9a
joeysciamanna <joeysciamanna@icloud.com>
2019-11-23 22:51:05 +0100

    TaskList

---
+++ src/main/java/ch/g_7/util/task/TaskList.java
@@ -0,0 +1,28 @@
+package ch.g_7.util.task;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import ch.g_7.util.task.Task.VoidTask;
+
+public class TaskList<I> implements VoidTask<I> {
+
+	private List<Task<I, Void>> tasks;
+
+	public TaskList() {
+		this.tasks = new ArrayList<Task<I, Void>>();
+	}
+
+	public void runAsync(I i) {
+		CompletableFuture.runAsync(() -> {
+			runVoid(i);
+		});
+	}
+
+	@Override
+	public void runVoid(I i) {
+		tasks.forEach((t) -> t.run(null));
+	}
+
+}
