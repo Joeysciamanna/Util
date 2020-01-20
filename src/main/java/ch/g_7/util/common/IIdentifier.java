ef4bc36ff863c051bcfb06102a4683ab6aeb5ddb
joeysciamanna <joeysciamanna@icloud.com>
2020-01-20 20:15:54 +0100

    Identifier

---
+++ src/main/java/ch/g_7/util/common/IIdentifier.java
@@ -0,0 +1,24 @@
+package ch.g_7.util.common;
+
+import java.lang.Enum.EnumDesc;
+import java.util.Optional;
+
+
+/**
+ * An Identifier with can only be implemented by an Enum.
+ * 
+ * @author Joey Sciamanna
+ * @param <E> Itself
+ */
+public interface IIdentifier<E extends Enum<E> & IIdentifier<E>> {
+
+	int compareTo(E o);
+	
+	Optional<EnumDesc<E>> describeConstable();
+
+	Class<E> getDeclaringClass();
+	
+	String name();
+	
+	int ordinal();
+}
