580a6c80a98ffa55f32d63082c27fe8636b1050c
joeysciamanna <joeysciamanna@icloud.com>
2020-02-13 15:27:05 +0100

    missing method

--- src/main/java/ch/g_7/util/resource/Resource.java
+++ src/main/java/ch/g_7/util/resource/Resource.java
@@ -59,5 +59,10 @@ public abstract class Resource implements IResource, IDepender {
 	public int hashCode() {
 		return Objects.hash(resourceId);
 	}
+
+	@Override
+	public int getResourceId() {
+		return resourceId;
+	}
 }
 
