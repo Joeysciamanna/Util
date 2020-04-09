883d6a96fd22c7bfc08728c13742f77933505050
joeysciamanna <joeysciamanna@icloud.com>
2020-04-09 14:00:12 +0200

    Util ifNotNull

--- src/main/java/ch/g_7/util/helper/Util.java
+++ src/main/java/ch/g_7/util/helper/Util.java
@@ -2,6 +2,7 @@ package ch.g_7.util.helper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -20,46 +21,13 @@ public class Util {
     	return true;
     }
     
-    
-    @SafeVarargs
-    public static <T> T[] genArrayOfNullables(Supplier<T>... suppliers){
-        List<T> ts = new ArrayList<>();
-        for (Supplier<T> supplier : suppliers) {
-            try {
-                ts.add(supplier.get());
-            }catch (NullPointerException e){ }
-        }
-        @SuppressWarnings("unchecked")
-		T[] array = (T[]) new Object[ts.size()];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = ts.get(i);
-        }
-        return array;
-    }
 
-    
-    public static int[] toUnsignedBytes(byte[] bytes) {
-    	int[] uBytes = new int[bytes.length];
-		for (int i = 0; i < bytes.length; i++) {
-			uBytes[i] = bytes[i] & 0xff;
-		}
-		return uBytes;
-    }
-    
-    public static byte[] toSignedBytes(int[] uBytes) {
-    	byte[] bytes = new byte[uBytes.length];
-		for (int i = 0; i < uBytes.length; i++) {
-			bytes[i] = (byte) uBytes[i];
-		}
-		return bytes;
-    }
-    
-    public static <T, K> K[] mapArray(T[] from, K[] to, Function<T, K> mapper) {
-		for (int i = 0; i < from.length; i++) {
-			to[i] = mapper.apply(from[i]);
-		}
-    	return to;
-    }
+    public static <T> void ifNotNull(T t, Consumer<T> consumer){
+    	if(t != null){
+			consumer.accept(t);
+    	}
+	}
+
     
 	@SuppressWarnings("unchecked")
 	public static <T> T cast(Object object) {
